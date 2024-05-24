8f7c8352ce436dc9c639539aa53728dab3d8b788
gustavcc <ritacardoso290@gmail.com>
2024-05-24 10:35:51 -0300

    up pdf

--- ReadPDF/main.py
+++ ReadPDF/main.py
@@ -1,8 +1,27 @@
 import PyPDF2
+import re
+import tabula
 
-enem_bin = open('ENEM2022.pdf', 'rb')
+pdf_file = open("pf1n3-2016.pdf", "rb")
 
-pdf = PyPDF2.PdfFileReader(enem_bin)
+# le o pdf
+pdf = PyPDF2.PdfReader(pdf_file)
 
-num_pag = pdf.getNumPages()
-print(num_pag)
+paginas = len(pdf.pages)
+# print(paginas)
+
+# extrair da primeira pagina
+pagina1 = pdf.pages[0]
+
+text_fomatado_page1 = ''.join(pagina1.extract_text())
+# print(text_fomatado_page1)
+
+# questoes separadas
+list_separados = []
+for index in range(1,5):
+    print(index)
+    text_pagina1_separado = text_fomatado_page1.split(f"{index}.")
+    list_separados.append(text_pagina1_separado)
+for i in lis:
+    print(i)
+    print('------------------------------------------------------------------')
